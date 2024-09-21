92eb3159aa9a8e70c971b90797a861786508a365
Haritha Mohan <harithamohan@microsoft.com>
2024-09-20 20:05:20 -0400

    add field support

---
+++ src/Xamarin.Macios.Generator/Xamarin.Macios.Generator/Parsers/FieldData.cs
@@ -0,0 +1,7 @@
+namespace Xamarin.Macios.Generator.Parsers;
+
+public class FieldData
+{
+    public string SymbolName { get; set; } = string.Empty;
+    public string LibraryName { get; set; } = string.Empty;
+}
